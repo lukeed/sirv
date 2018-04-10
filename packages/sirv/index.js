07246fe5f49e1747bb041cfd6e60c087143c5acc
Luke Edwards <luke.edwards05@gmail.com>
2018-04-10 12:17:46 -0700

    use existing `path(name)` for lookup else parse it

--- packages/sirv/index.js
+++ packages/sirv/index.js
@@ -1,9 +1,11 @@
 const http2 = require('http2');
+const parse = require('parseurl');
+
 module.exports = function (dir, opts={}) {
 	if (!opts.key || !opts.cert) {
 		throw new Error('HTTP/2 requires "key" and "cert" values!');
 	}
 	return http2.createSecureServer(opts, (req, res) => {
-		//
+		let name = req.path || req.pathname || parse(req).pathname;
 	});
 }
