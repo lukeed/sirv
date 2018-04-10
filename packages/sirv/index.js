91b8cda61fc813dfc58325ed85620c509ceb5655
Luke Edwards <luke.edwards05@gmail.com>
2018-04-10 12:04:39 -0700

    throw early if no `key` or `cert` values

--- packages/sirv/index.js
+++ packages/sirv/index.js
@@ -1,5 +1,8 @@
 const http2 = require('http2');
 module.exports = function (dir, opts={}) {
+	if (!opts.key || !opts.cert) {
+		throw new Error('HTTP/2 requires "key" and "cert" values!');
+	}
 	return http2.createSecureServer(opts, (req, res) => {
 		//
 	});
