38d3e520425660d8e6708e54ab43e98345bb864c
Luke Edwards <luke.edwards05@gmail.com>
2018-04-10 21:11:53 -0700

    add `extensions` & `onNoMatch` opts w/ defaults

--- packages/sirv/index.js
+++ packages/sirv/index.js
@@ -5,6 +5,8 @@ module.exports = function (dir, opts={}) {
 	if (!opts.key || !opts.cert) {
 		throw new Error('HTTP/2 requires "key" and "cert" values!');
 	}
+	let extensions = opts.extensions || ['html', 'htm'];
+	let onNoMatch = opts.onNoMatch || res => (res.statusCode=404,res.end());
 	return http2.createSecureServer(opts, (req, res) => {
 		let name = req.path || req.pathname || parse(req).pathname;
 	});
