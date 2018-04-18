ed6cf05ef56a28462e0a08606d21734d70998d00
Luke Edwards <luke.edwards05@gmail.com>
2018-04-18 00:06:31 -0700

    support HTTP/1 only;
    - use regular `http` server
    - use sync version of `tiny-glob`
    - pipe file descriptor into response stream
    - add etag, immutable, & maxAge options
    - support custom “setHeaders” fn

--- packages/sirv/index.js
+++ packages/sirv/index.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
-const http2 = require('http2');
 const { join } = require('path');
+const { createServer } = require('http');
+const tglob = require('tiny-glob/sync');
 const parseurl = require('parseurl');
-const tglob = require('tiny-glob');
 const mime = require('mime/lite');
 
 const FILES = {};
 const noop = () => {};
-const { HTTP2_HEADER_PATH } = http2.constants;
 
 // opts = {
 // 	// etag,
@@ -20,25 +19,10 @@ const { HTTP2_HEADER_PATH } = http2.constants;
 // 	// onNoMatch
 // }
 
-function push(stream, path) {
-	let d = FILES[path.substring(1)];
-	if (!d) return Promise.resolve();
-	return new Promise((res, rej) => {
-		stream.pushStream({ [HTTP2_HEADER_PATH]: path }, (err, pstream) => {
-			pstream.once('error', rej).once('finish', res);
-			pstream.respondWithFD(d.descriptor, d.headers);
-		});
-	});
-}
-
 function find(uri, extns) {
-	uri = uri.substring(1);
+	if (!!~uri.lastIndexOf('.')) return FILES[uri];
 
-	if (!!~uri.lastIndexOf('.')) {
-		return { file:uri, data:FILES[uri] };
-	}
-
-	let i, j, x, arr=[], file, data, len=uri.length-1;
+	let i, j, x, arr=[], data, len=uri.length-1;
 	if (uri.charCodeAt(len) === 47) uri=uri.substring(0, len);
 
 	let tmp = uri ? [uri, `${uri}/index`] : ['index'];
@@ -50,65 +34,47 @@ function find(uri, extns) {
 	}
 
 	for (i=0; i < arr.length; i++) {
-		if (data=FILES[file=arr[i]]) break;
+		if (data=FILES[arr[i]]) break;
 	}
 
-	return { file, data };
+	return data;
 }
 
-// TODO: CLI prompt to create local/dev SSL if empty
-module.exports = async function (dir, opts={}) {
-	if (!opts.key || !opts.cert) {
-		throw new Error('HTTP/2 requires "key" and "cert" values!');
-	}
+function toEtag(obj) {
+	return `W/"${obj.size.toString(16)}-${obj.mtime.getTime().toString(16)}"`;
+}
 
-	let headers = {}; // etag, cache-control
+module.exports = function (dir, opts={}) {
+	let cc = opts.maxAge && `public,max-age=${opts.maxAge}`;
+	cc && opts.immutable && (cc += ',immutable');
 
 	opts.cwd = dir;
-	// stash(dir, files); ?
-	await tglob('**/*.*', opts).then(arr => {
-		let stats, tunnel;
-		arr.forEach(str => {
-			tunnel = fs.openSync(join(dir, str), 'r');
-			stats = fs.fstatSync(tunnel);
-			FILES[str] = {
-				descriptor: tunnel,
-				headers: {
-					...headers,
-					'content-length': stats.size,
-					'content-type': mime.getType(str),
-					'last-modified': stats.mtime.toUTCString()
-				}
-			};
-		});
+	let fd, stats, headers;
+	tglob('**/*.*', opts).forEach(str => {
+		fd = fs.openSync(join(dir, str), 'r');
+		stats = fs.fstatSync(fd);
+		headers = {
+			'content-length': stats.size,
+			'content-type': mime.getType(str),
+			'last-modified': stats.mtime.toUTCString()
+		};
+		cc && (headers['cache-control'] = cc);
+		opts.etag && (headers['etag'] = toEtag(stats));
+		FILES['/' + str] = { fd, stats, headers };
 	});
 
-	let server = http2.createSecureServer(opts);
+	let setHeaders = opts.setHeaders || noop;
 	let extensions = opts.extensions || ['html', 'htm'];
 	let onNoMatch = opts.onNoMatch || (res => (res.statusCode=404,res.end()));
-	let manifest = opts.manifest || {};
 
-	server.on('stream', async (stream, headers) => {
-		// todo (fallback): req.stream.session vs req
-		// let isPush = req.httpVersion === '2.0';
-		// opts.allowHTTP1 = true;
-
-		let url = headers[HTTP2_HEADER_PATH];
-		let { pathname } = parseurl({ url });
-		// let pathname = req.path || req.pathname || parseurl(req).pathname;
-		let { data } = find(pathname, extensions);
+	return createServer(opts, (req, res) => {
+		let pathname = req.path || req.pathname || parseurl(req).pathname;
+		let data = find(pathname, extensions);
 		if (!data) return onNoMatch(res);
 
-		// Look for assets to Push
-		let assets = manifest[pathname];
-		if (stream.pushAllowed && assets !== void 0) {
-			let k, fns=[];
-			for (k in assets) fns.push(push(stream, k));
-			await Promise.all(fns);
-		}
+		res.writeHead(200, data.headers);
+		setHeaders(res, pathname, data.stats);
 
-		stream.respondWithFD(data.descriptor, data.headers);
+		fs.createReadStream({ fd:data.fd }).pipe(res);
 	});
-
-	return server;
 }
