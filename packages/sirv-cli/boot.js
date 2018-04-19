2be0a5f739033f1fd2a8f8e6905582d75ceedec3
Luke Edwards <luke.edwards05@gmail.com>
2018-04-19 00:18:21 -0700

    colorize log items ; add “LOGS” title divider

--- packages/sirv-cli/boot.js
+++ packages/sirv-cli/boot.js
@@ -5,8 +5,15 @@ const clear = require('console-clear');
 const { find } = require('port-authority');
 const access = require('local-access');
 
+const PAD = '  ';
+
 function toMS(arr) {
-	return `${(arr[1] / 1e6).toFixed(2)}ms`;
+	return colors.white.bold(`${(arr[1] / 1e6).toFixed(2)}ms`);
+}
+
+function toCode(code) {
+	let fn = code > 400 ? 'red' : code > 300 ? 'yellow' : 'green';
+	return colors[fn](code);
 }
 
 module.exports = function (dir, opts) {
@@ -16,11 +23,13 @@ module.exports = function (dir, opts) {
 	let { hrtime, stdout } = process;
 
 	if (!opts.quiet) {
+		let uri, dur, start, dash=colors.gray(' ─ ');
 		server.on('request', (req, res) => {
-			let dur, start=hrtime();
+			start = hrtime();
 			req.once('end', _ => {
 				dur = hrtime(start);
-				stdout.write(`[${res.statusCode}] — ${toMS(dur)} — ${req.originalUrl || req.url}\n`);
+				uri = req.originalUrl || req.url;
+				stdout.write(PAD + toCode(res.statusCode) + dash + toMS(dur) + dash + uri + '\n');
 			});
 		});
 	}
@@ -33,12 +42,13 @@ module.exports = function (dir, opts) {
 			if (opts.quiet) return;
 
 			clear(); // wipe screen
-			let PAD = '  ';
 			let { local, network } = access({ port, https });
 			stdout.write('\n' + PAD + colors.green('Your application is ready~! 🚀\n\n'));
 			isOther && stdout.write(PAD + colors.italic.dim(`➡ Port ${opts.port} is taken; using ${port} instead\n\n`));
 			stdout.write(PAD + `${colors.bold('- Local:')}      ${local}\n`);
 			stdout.write(PAD + `${colors.bold('- Network:')}    ${network}\n`);
+			let border = '─'.repeat(Math.min(stdout.columns, 36) / 2);
+			stdout.write('\n' + border + colors.inverse(' LOGS ') + border + '\n\n');
 		});
 	});
 }
