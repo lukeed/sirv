efdf7833ba3f5c00026699d29bf87708bf0e46e0
Luke Edwards <luke.edwards05@gmail.com>
2018-08-20 15:35:41 -0700

    extract `toAssume` from `find` helper

--- packages/sirv/index.js
+++ packages/sirv/index.js
@@ -7,23 +7,26 @@ const mime = require('mime/lite');
 const FILES = {};
 const noop = () => {};
 
-function find(uri, extns) {
-	if (!!~uri.lastIndexOf('.')) return FILES[uri];
-
-	let i, j, x, arr=[], data, len=uri.length-1;
+function toAssume(uri, extns) {
+	let i=0, x, len=uri.length - 1;
 	if (uri.charCodeAt(len) === 47) uri=uri.substring(0, len);
 
-	let tmp = `${uri}/index`;
-	for (i=0; i < extns.length; i++) {
+	let arr=[], tmp=`${uri}/index`;
+	for (; i < extns.length; i++) {
 		x = '.' + extns[i];
 		if (uri) arr.push(uri + x);
 		arr.push(tmp + x);
 	}
 
-	for (i=0; i < arr.length; i++) {
+	return arr;
+}
+
+function find(uri, extns) {
+	if (!!~uri.lastIndexOf('.')) return FILES[uri];
+	let i=0, data, arr=toAssume(uri, extns);
+	for (; i < arr.length; i++) {
 		if (data=FILES[arr[i]]) break;
 	}
-
 	return data;
 }
 
