061e8a1c3eac9894c17f397e5dacf2da41c9c969
Luke Edwards <luke.edwards05@gmail.com>
2020-05-23 14:49:44 -0700

    chore: re-extract `middleware` helper file

--- tests/sirv.js
+++ tests/sirv.js
@@ -1,38 +1,7 @@
-const { test } = require('uvu');
-const assert = require('uvu/assert');
-const { Writable } = require('stream');
-const sirv = require('../packages/sirv');
-
-async function runMiddleware(fn, req) {
-	const out = {
-		headers: {},
-		statusCode: -1,
-	}
-
-	await new Promise((resolve, reject) => {
-		const res = new Writable({
-			write() {}
-		});
-		Object.defineProperty(res, 'statusCode', {
-			set(value) {
-				out.statusCode = value;
-			}
-		})
-		res.on('error', reject)
-		res.on('finish', resolve);
-		res.writeHead = (code, headers) => {
-			out.statusCode = code;
-			Object.assign(out.headers, headers);
-		}
-		fn(req, res);
-	});
-
-	return out;
-}
 import { test } from 'uvu';
 import assert from 'uvu/assert';
-import { Writable } from 'stream';
 import sirv from '../packages/sirv';
+import { middleware } from './helpers';
 
 test('exports', () => {
 	assert.type(sirv, 'function');
@@ -41,7 +10,7 @@ test('exports', () => {
 test('prevents directory traversal attacks :: prod', async () => {
 	const handler = sirv(__dirname, { dev: false });
 
-	const res = await runMiddleware(handler, {
+	const res = await middleware(handler, {
 		headers: {},
 		path: encodeURIComponent('../package.json'),
 	});
@@ -52,7 +21,7 @@ test('prevents directory traversal attacks :: prod', async () => {
 test('prevents directory traversal attacks :: dev', async () => {
 	const handler = sirv(__dirname, { dev: true });
 
-	const res = await runMiddleware(handler, {
+	const res = await middleware(handler, {
 		headers: {},
 		path: encodeURIComponent('../package.json'),
 	});
